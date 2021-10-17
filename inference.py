2b3f73b05f2c988fb799ee3316249e3d417bf3b1
Sathvik Udupa <sathvikudupa66@gmail.com>
2021-10-17 17:44:28 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -34,32 +34,21 @@ def scan_checkpoint(cp_dir, prefix):
     return sorted(cp_list)[-1]
 
 
-def inference(a):
+def inference(checkpoint_file, device, ):
     generator = Generator(h).to(device)
 
-    state_dict_g = load_checkpoint(a.checkpoint_file, device)
+    state_dict_g = load_checkpoint(checkpoint_file, device)
     generator.load_state_dict(state_dict_g['generator'])
-
-    filelist = os.listdir(a.input_wavs_dir)
-
-    os.makedirs(a.output_dir, exist_ok=True)
-
     generator.eval()
     generator.remove_weight_norm()
     with torch.no_grad():
-        for i, filname in enumerate(filelist):
-            wav, sr = load_wav(os.path.join(a.input_wavs_dir, filname))
-            wav = wav / MAX_WAV_VALUE
-            wav = torch.FloatTensor(wav).to(device)
-            x = get_mel(wav.unsqueeze(0))
-            y_g_hat = generator(x)
-            audio = y_g_hat.squeeze()
-            audio = audio * MAX_WAV_VALUE
-            audio = audio.cpu().numpy().astype('int16')
-
-            output_file = os.path.join(a.output_dir, os.path.splitext(filname)[0] + '_generated.wav')
-            write(output_file, h.sampling_rate, audio)
-            print(output_file)
+        x = torch.from_numpy(x)
+        y_g_hat = generator(x)
+        audio = y_g_hat.squeeze()
+        audio = audio * MAX_WAV_VALUE
+        audio = audio.cpu().numpy().astype('int16')
+    return audio
+           
 
 
 def main():
